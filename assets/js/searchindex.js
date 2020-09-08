0e605f4387b45e42c6427070b0b096e4e938f39d
$env:op_build_user <$env:op_build_user_email>
2020-09-08 19:13:17 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,118 +10,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Playing Service Workers",
-            content:"Service Workers",
-            description:'',
-            tags:'Service Workers'
-        },
-        {
-            url:'/posts/Playing-with-Service-Workers',
-            title:"Playing with Service Workers",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
-            title:"Running ASP NET Core RaspberryPi Nginx",
-            content:"ASP NET Core",
-            description:'',
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/Running%20ASP.NET%20Core%20on%20a%20RaspberryPi%202%20with%20Nginx',
-            title:"Running ASP.NET Core on a RaspberryPi 2 with Nginx",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
-            title:"Setting up Raspberry Pi NGINX PHP MySQL LEMP Stack",
-            content:"Raspberry Pi",
-            description:'',
-            tags:'Raspberry Pi'
-        },
-        {
-            url:'/posts/Setting-Raspberry-Pi-NGINX-PHP-MySQL-LEMP-Stack',
-            title:"Setting up a Raspberry Pi NGINX PHP MySQL LEMP Stack",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"Using Netlify Identity NetlifyCMS Wyam",
-            content:"Wyam",
-            description:"test lead",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part%202',
-            title:"Using Netlify Identity with NetlifyCMS and Wyam",
-            description:"test a lead"
-        }
-    );
-    a(
-        {
-            id:4,
-            title:"Setting up NetlifyCMS Wyam Part",
-            content:"Wyam",
-            description:"Configuring simple single user mode NetlifyCMS Wyam",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-1',
-            title:"Setting up NetlifyCMS with Wyam - Part 1",
-            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
-        }
-    );
-    a(
-        {
-            id:5,
-            title:"Using Netlify Identity NetlifyCMS Wyam",
-            content:"Wyam",
-            description:"test lead",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-4',
-            title:"Using Netlify Identity with NetlifyCMS and Wyam",
-            description:"test a lead"
-        }
-    );
-    a(
-        {
-            id:6,
-            title:"Setting up NetlifyCMS Wyam Part",
-            content:"Wyam",
-            description:"Configuring simple single user mode NetlifyCMS Wyam",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-3',
-            title:"Setting up NetlifyCMS with Wyam - Part 3",
-            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
-        }
-    );
-    a(
-        {
-            id:7,
-            title:"VSTO installs over HTTPS issues",
-            content:"VSTO",
-            description:'',
-            tags:'VSTO'
-        },
-        {
-            url:'/posts/VSTO-installs-over-HTTPS-issues',
-            title:"VSTO installs over HTTPS issues",
-            description:""
-        }
-    );
-    a(
-        {
-            id:8,
             title:"Secure ASP NET Core Health Checks specific port",
             content:"ASP NET Core",
             description:"Health Checks ASP NET Core give valuable insights health services includes sensitive data leaked externally",
@@ -135,21 +23,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:9,
-            title:"Test blog post docs",
-            content:"NetlifyCMS",
-            description:"blog post written NetlifyCMS",
-            tags:'NetlifyCMS'
-        },
-        {
-            url:'/posts/2018-02-11-test-blog-post-for-docs',
-            title:"Test blog post for docs",
-            description:"This blog post was written in NetlifyCMS"
-        }
-    );
-    a(
-        {
-            id:10,
+            id:1,
             title:"fork cloned repository Visual Studio",
             content:"GitHub",
             description:'',
@@ -163,21 +37,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:11,
-            title:"Secure Swagger ASP NET Core address port",
-            content:"ASP NET Core",
-            description:"Swagger powerful tool test APIs allow users easily discover consume APIs open up security issues make easier attackers access data",
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/2020-09-08-secure-swagger-on-asp-net-core-by-address-and-port',
-            title:"Secure Swagger on ASP.NET Core by address and port",
-            description:"Swagger is a powerful tool to test your APIs and allow users to easily discover how to consume your APIs, but it can also open up security issues and make it easier for attackers to access your data."
-        }
-    );
-    a(
-        {
-            id:12,
+            id:2,
             title:"Job Interview Technical Test Preparation",
             content:"ASP NET Core",
             description:'',
@@ -191,21 +51,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:13,
-            title:"First Post",
-            content:"Introduction",
-            description:'',
-            tags:'Introduction'
-        },
-        {
-            url:'/posts/miniblog%20clone',
-            title:"First Post",
-            description:""
-        }
-    );
-    a(
-        {
-            id:14,
+            id:3,
             title:"Resolving System NotSupportedException data available encoding net Core",
             content:"NET Core",
             description:'',
@@ -219,7 +65,105 @@ var searchModule = function() {
     );
     a(
         {
-            id:15,
+            id:4,
+            title:"Playing Service Workers",
+            content:"Service Workers",
+            description:'',
+            tags:'Service Workers'
+        },
+        {
+            url:'/posts/Playing-with-Service-Workers',
+            title:"Playing with Service Workers",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
+            title:"Setting up Raspberry Pi NGINX PHP MySQL LEMP Stack",
+            content:"Raspberry Pi",
+            description:'',
+            tags:'Raspberry Pi'
+        },
+        {
+            url:'/posts/Setting-Raspberry-Pi-NGINX-PHP-MySQL-LEMP-Stack',
+            title:"Setting up a Raspberry Pi NGINX PHP MySQL LEMP Stack",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"Setting up NetlifyCMS Wyam Part",
+            content:"Wyam",
+            description:"Configuring simple single user mode NetlifyCMS Wyam",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-1',
+            title:"Setting up NetlifyCMS with Wyam - Part 1",
+            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
+        }
+    );
+    a(
+        {
+            id:7,
+            title:"Using Netlify Identity NetlifyCMS Wyam",
+            content:"Wyam",
+            description:"test lead",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-4',
+            title:"Using Netlify Identity with NetlifyCMS and Wyam",
+            description:"test a lead"
+        }
+    );
+    a(
+        {
+            id:8,
+            title:"Test blog post docs",
+            content:"NetlifyCMS",
+            description:"blog post written NetlifyCMS",
+            tags:'NetlifyCMS'
+        },
+        {
+            url:'/posts/2018-02-11-test-blog-post-for-docs',
+            title:"Test blog post for docs",
+            description:"This blog post was written in NetlifyCMS"
+        }
+    );
+    a(
+        {
+            id:9,
+            title:"Secure Swagger ASP NET Core address port",
+            content:"ASP NET Core",
+            description:"Swagger powerful tool test APIs allow users easily discover consume APIs open up security issues make easier attackers access data",
+            tags:'ASP.NET Core'
+        },
+        {
+            url:'/posts/2020-09-08-secure-swagger-on-asp-net-core-by-address-and-port',
+            title:"Secure Swagger on ASP.NET Core by address and port",
+            description:"Swagger is a powerful tool to test your APIs and allow users to easily discover how to consume your APIs, but it can also open up security issues and make it easier for attackers to access your data."
+        }
+    );
+    a(
+        {
+            id:10,
+            title:"First Post",
+            content:"Introduction",
+            description:'',
+            tags:'Introduction'
+        },
+        {
+            url:'/posts/miniblog%20clone',
+            title:"First Post",
+            description:""
+        }
+    );
+    a(
+        {
+            id:11,
             title:"NUnit Tests Showing Visual Studio Test Explorer",
             content:"NUnit",
             description:'',
@@ -228,6 +172,62 @@ var searchModule = function() {
         {
             url:'/posts/Nunit_Tests_Not_Showing_In_Test_Explorer',
             title:"NUnit 3 Tests Are Not Showing In Visual Studio Test Explorer",
+            description:""
+        }
+    );
+    a(
+        {
+            id:12,
+            title:"Running ASP NET Core RaspberryPi Nginx",
+            content:"ASP NET Core",
+            description:'',
+            tags:'ASP.NET Core'
+        },
+        {
+            url:'/posts/Running%20ASP.NET%20Core%20on%20a%20RaspberryPi%202%20with%20Nginx',
+            title:"Running ASP.NET Core on a RaspberryPi 2 with Nginx",
+            description:""
+        }
+    );
+    a(
+        {
+            id:13,
+            title:"Using Netlify Identity NetlifyCMS Wyam",
+            content:"Wyam",
+            description:"test lead",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part%202',
+            title:"Using Netlify Identity with NetlifyCMS and Wyam",
+            description:"test a lead"
+        }
+    );
+    a(
+        {
+            id:14,
+            title:"Setting up NetlifyCMS Wyam Part",
+            content:"Wyam",
+            description:"Configuring simple single user mode NetlifyCMS Wyam",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-3',
+            title:"Setting up NetlifyCMS with Wyam - Part 3",
+            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
+        }
+    );
+    a(
+        {
+            id:15,
+            title:"VSTO installs over HTTPS issues",
+            content:"VSTO",
+            description:'',
+            tags:'VSTO'
+        },
+        {
+            url:'/posts/VSTO-installs-over-HTTPS-issues',
+            title:"VSTO installs over HTTPS issues",
             description:""
         }
     );
