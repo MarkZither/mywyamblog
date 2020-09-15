cccbfdb8a9856e9d0ca05f7d0cd4e85763eba84d
$env:op_build_user <$env:op_build_user_email>
2020-09-15 07:48:41 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,20 +10,76 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"NUnit Tests Showing Visual Studio Test Explorer",
-            content:"NUnit",
-            description:'',
-            tags:'NUnit'
+            title:"Secure ASP NET Core Health Checks specific port",
+            content:"ASP NET Core",
+            description:"Health Checks ASP NET Core give valuable insights health services includes sensitive data leaked externally",
+            tags:'ASP.NET Core'
         },
         {
-            url:'/posts/Nunit_Tests_Not_Showing_In_Test_Explorer',
-            title:"NUnit 3 Tests Are Not Showing In Visual Studio Test Explorer",
-            description:""
+            url:'/posts/2020-09-08-Secure-ASP.NET-Core-Health-Checks-to-a-specific-port',
+            title:"Secure ASP.NET Core Health Checks to a specific port",
+            description:"Health Checks in ASP.NET Core can give valuable insights into the health of services, but that includes sensitive data that should not be leaked externally."
         }
     );
     a(
         {
             id:1,
+            title:"Experimenting YARP Reverse Proxy",
+            content:"ASP NET Core",
+            description:"YARP reverse proxy toolkit building fast proxy servers NET using infrastructure ASP NET NET",
+            tags:'ASP.NET Core'
+        },
+        {
+            url:'/posts/2020-09-14-experimenting-with-yarp-a-reverse-proxy',
+            title:"Experimenting with YARP: A Reverse Proxy",
+            description:"YARP is a reverse proxy toolkit for building fast proxy servers in .NET using the infrastructure from ASP.NET and .NET."
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"Job Interview Technical Test Preparation",
+            content:"ASP NET Core",
+            description:'',
+            tags:'ASP.NET Core'
+        },
+        {
+            url:'/posts/Job%20Interview%20Technical%20Test%20Preparation',
+            title:"Job Interview Technical Test Preparation",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"Resolving System NotSupportedException data available encoding net Core",
+            content:"NET Core",
+            description:'',
+            tags:'.NET Core'
+        },
+        {
+            url:'/posts/NotSupportedException%20exception%20using%20Encoding%20in%20.net%20core',
+            title:"Resolving System.NotSupportedException No data is available for encoding 850 in .net Core",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"Playing Service Workers",
+            content:"Service Workers",
+            description:'',
+            tags:'Service Workers'
+        },
+        {
+            url:'/posts/Playing-with-Service-Workers',
+            title:"Playing with Service Workers",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
             title:"Setting up Raspberry Pi NGINX PHP MySQL LEMP Stack",
             content:"Raspberry Pi",
             description:'',
@@ -37,49 +93,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
-            title:"Running ASP NET Core RaspberryPi Nginx",
-            content:"ASP NET Core",
-            description:'',
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/Running%20ASP.NET%20Core%20on%20a%20RaspberryPi%202%20with%20Nginx',
-            title:"Running ASP.NET Core on a RaspberryPi 2 with Nginx",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"VSTO installs over HTTPS issues",
-            content:"VSTO",
-            description:'',
-            tags:'VSTO'
-        },
-        {
-            url:'/posts/VSTO-installs-over-HTTPS-issues',
-            title:"VSTO installs over HTTPS issues",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
-            title:"Using Netlify Identity NetlifyCMS Wyam",
-            content:"Wyam",
-            description:"test lead",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part%202',
-            title:"Using Netlify Identity with NetlifyCMS and Wyam",
-            description:"test a lead"
-        }
-    );
-    a(
-        {
-            id:5,
+            id:6,
             title:"Setting up NetlifyCMS Wyam Part",
             content:"Wyam",
             description:"Configuring simple single user mode NetlifyCMS Wyam",
@@ -88,20 +102,6 @@ var searchModule = function() {
         {
             url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-1',
             title:"Setting up NetlifyCMS with Wyam - Part 1",
-            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
-        }
-    );
-    a(
-        {
-            id:6,
-            title:"Setting up NetlifyCMS Wyam Part",
-            content:"Wyam",
-            description:"Configuring simple single user mode NetlifyCMS Wyam",
-            tags:'Wyam'
-        },
-        {
-            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-3',
-            title:"Setting up NetlifyCMS with Wyam - Part 3",
             description:"Configuring a simple single user mode NetlifyCMS with Wyam"
         }
     );
@@ -122,20 +122,6 @@ var searchModule = function() {
     a(
         {
             id:8,
-            title:"Secure ASP NET Core Health Checks specific port",
-            content:"ASP NET Core",
-            description:"Health Checks ASP NET Core give valuable insights health services includes sensitive data leaked externally",
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/2020-09-08-Secure-ASP.NET-Core-Health-Checks-to-a-specific-port',
-            title:"Secure ASP.NET Core Health Checks to a specific port",
-            description:"Health Checks in ASP.NET Core can give valuable insights into the health of services, but that includes sensitive data that should not be leaked externally."
-        }
-    );
-    a(
-        {
-            id:9,
             title:"Test blog post docs",
             content:"NetlifyCMS",
             description:"blog post written NetlifyCMS",
@@ -149,21 +135,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:10,
-            title:"Experimenting YARP Reverse Proxy",
-            content:"ASP NET Core",
-            description:"YARP reverse proxy toolkit building fast proxy servers NET using infrastructure ASP NET NET",
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/2020-09-14-experimenting-with-yarp-a-reverse-proxy',
-            title:"Experimenting with YARP: A Reverse Proxy",
-            description:"YARP is a reverse proxy toolkit for building fast proxy servers in .NET using the infrastructure from ASP.NET and .NET."
-        }
-    );
-    a(
-        {
-            id:11,
+            id:9,
             title:"Secure Swagger ASP NET Core address port",
             content:"ASP NET Core",
             description:'',
@@ -177,21 +149,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:12,
-            title:"Job Interview Technical Test Preparation",
-            content:"ASP NET Core",
-            description:'',
-            tags:'ASP.NET Core'
-        },
-        {
-            url:'/posts/Job%20Interview%20Technical%20Test%20Preparation',
-            title:"Job Interview Technical Test Preparation",
-            description:""
-        }
-    );
-    a(
-        {
-            id:13,
+            id:10,
             title:"fork cloned repository Visual Studio",
             content:"GitHub",
             description:'',
@@ -205,21 +163,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:14,
-            title:"Resolving System NotSupportedException data available encoding net Core",
-            content:"NET Core",
-            description:'',
-            tags:'.NET Core'
-        },
-        {
-            url:'/posts/NotSupportedException%20exception%20using%20Encoding%20in%20.net%20core',
-            title:"Resolving System.NotSupportedException No data is available for encoding 850 in .net Core",
-            description:""
-        }
-    );
-    a(
-        {
-            id:15,
+            id:11,
             title:"First Post",
             content:"Introduction",
             description:'',
@@ -233,15 +177,71 @@ var searchModule = function() {
     );
     a(
         {
-            id:16,
-            title:"Playing Service Workers",
-            content:"Service Workers",
+            id:12,
+            title:"NUnit Tests Showing Visual Studio Test Explorer",
+            content:"NUnit",
             description:'',
-            tags:'Service Workers'
+            tags:'NUnit'
         },
         {
-            url:'/posts/Playing-with-Service-Workers',
-            title:"Playing with Service Workers",
+            url:'/posts/Nunit_Tests_Not_Showing_In_Test_Explorer',
+            title:"NUnit 3 Tests Are Not Showing In Visual Studio Test Explorer",
+            description:""
+        }
+    );
+    a(
+        {
+            id:13,
+            title:"Running ASP NET Core RaspberryPi Nginx",
+            content:"ASP NET Core",
+            description:'',
+            tags:'ASP.NET Core'
+        },
+        {
+            url:'/posts/Running%20ASP.NET%20Core%20on%20a%20RaspberryPi%202%20with%20Nginx',
+            title:"Running ASP.NET Core on a RaspberryPi 2 with Nginx",
+            description:""
+        }
+    );
+    a(
+        {
+            id:14,
+            title:"Using Netlify Identity NetlifyCMS Wyam",
+            content:"Wyam",
+            description:"test lead",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part%202',
+            title:"Using Netlify Identity with NetlifyCMS and Wyam",
+            description:"test a lead"
+        }
+    );
+    a(
+        {
+            id:15,
+            title:"Setting up NetlifyCMS Wyam Part",
+            content:"Wyam",
+            description:"Configuring simple single user mode NetlifyCMS Wyam",
+            tags:'Wyam'
+        },
+        {
+            url:'/posts/Setting-up-NetlifyCMS-with-Wyam---Part-3',
+            title:"Setting up NetlifyCMS with Wyam - Part 3",
+            description:"Configuring a simple single user mode NetlifyCMS with Wyam"
+        }
+    );
+    a(
+        {
+            id:16,
+            title:"VSTO installs over HTTPS issues",
+            content:"VSTO",
+            description:'',
+            tags:'VSTO'
+        },
+        {
+            url:'/posts/VSTO-installs-over-HTTPS-issues',
+            title:"VSTO installs over HTTPS issues",
             description:""
         }
     );
