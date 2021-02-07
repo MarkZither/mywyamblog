0f54e8388c54d6603e2e4196ce4f09d3317f3dc6
MarkZither <mark.burton@zither-it.co.uk>
2021-02-07 11:04:34 +0100

    add user secrets config and netlify deploy

--- src/blog/Program.cs
+++ src/blog/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
 using MyStatiq.SearchIndex;
 using Statiq.App;
 using Statiq.Common;
@@ -14,13 +17,32 @@ namespace mywyamblog
 {
     class Program
     {
-        public static async Task<int> Main(string[] args) =>
-            await Bootstrapper
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddUserSecrets<Program>()
+            .Build();
+
+        public static async Task<int> Main(string[] args)
+        {
+            var config = Configuration;
+            List<KeyValuePair<string, object>> settings = new List<KeyValuePair<string, object>>();
+            foreach (var conf in Configuration.AsEnumerable())
+            {
+                settings.Add(new KeyValuePair<string, object>(conf.Key, conf.Value));
+            }
+            var bootstrapper = Bootstrapper
                 .Factory
                 .CreateWeb(args)
+                .BuildConfiguration(cfg => cfg.AddUserSecrets<Program>())
                 .AddSetting(
-                  CustomKeys.GenerateSearchIndex,
-                  true)
+                CustomKeys.GenerateSearchIndex,
+                true)
+                .AddSettings(settings)
+                .DeployToNetlify(Config.FromSetting<string>("NetlifySiteId"),
+                    Configuration.GetValue<string>("NetlifyAccessToken"));
+
+            return await bootstrapper
                 .RunAsync();
+        }
     }
 }
